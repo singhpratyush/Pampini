f6daf84903f4965bcdaf1293f90a5e6e1c2a1b55
Pratyush Singh <singh.pratyush96@gmail.com>
2016-03-02 23:58:25 +0530

    Added log in support.

--- src/database/operation/Update.java
+++ src/database/operation/Update.java
@@ -1,13 +1,32 @@
 package database.operation;
 
 import database.definition.Pampini_file;
+import util.config;
 
 import java.net.SocketAddress;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Update {
     //Mark user as online
     public static boolean mark_active(int uid, SocketAddress client_IP) {
-        return true;
+        try {
+            Connection c = DriverManager.getConnection(config.jdbc, config.jdbc_username, config.jdbc_password);
+            Statement stmt = c.createStatement();
+
+            String operation = "set search path to file;\n" +
+                    " insert into active_users values(" +
+                    uid + "," + client_IP.toString() +
+                    "0);";
+
+            stmt.executeUpdate(operation);
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     //Mark user as offline
